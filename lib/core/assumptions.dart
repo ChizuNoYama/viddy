2832797e5597c86f29716c0b32e23699996eac85
ChizuNoYama <titonton12144@hotmail.com>
2025-01-03 15:45:17 -0500

    cleaup: File rename to fix import referencing

---
+++ lib/core/assumptions.dart
@@ -0,0 +1,22 @@
+class Assumptions{
+  static const String SUPABASE_API_URL = "https://kkzzoqcqhuhdbtyuxozv.supabase.co";
+  static const String ID_KEY = "id";
+  static const String CONVERSATION_ID_KEY = "conversationId";
+  static const String USER_ID_KEY = "userId";
+  static const String USER_KEY = "user";
+  static const String USER_NAME_KEY = "userName";
+  static const String PAYLOAD_KEY = "payload";
+  static const String MESSAGE_KEY = "message";
+  static const String MESSAGE_LIST_KEY = "messageList";
+  static const String MESSAGE_TYPE_KEY = "messageType";
+  static const String PARTICIPANTS_KEY = "participants";
+  static const String SENDER_KEY = "sender";
+  static const String CREATED_AT_KEY = "created_at";
+
+  static const String CONVO_ID_PARAM = "convoId";
+
+  static String MESSAGE_SENT_EVENT = "message-sent";
+  static String MESSAGE_RECIEVED_EVENT = "message-recieved";
+
+  static String VIBRATE_DEVICE_METHOD = "vibrateDevice";
+}
